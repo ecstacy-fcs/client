9a69188dd8246f279206c075136da69965b9db86
Ananya Lohani <61914211+ananyalohani@users.noreply.github.com>
2021-09-29 14:04:47 +0530

    chore: Move code into src, set up tsconfig, eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  extends: ['next', 'next/core-web-vitals'],
+};
---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  semi: true,
+  trailingComma: 'es5',
+  singleQuote: true,
+  useTabs: false,
+  tabWidth: 2,
+  printWidth: 80,
+};
