474e23ff5f319bcb8c3702bed892148b63eac9f8
Ananya Lohani <61914211+ananyalohani@users.noreply.github.com>
2021-11-06 23:19:09 +0530

    feat: FileInput component

--- src/components/FileInput.tsx
+++ src/components/FileInput.tsx
@@ -1,5 +1,5 @@
-import { Button, Input } from "@chakra-ui/react";
-import React from "react";
+import { Button, VStack, Input, Stack, Text } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
 
 export interface IProps {
   acceptedFileTypes?: string;
@@ -12,41 +12,64 @@ export interface IProps {
 export const FileInput: React.FC<IProps> = (props) => {
   const fileInputRef = React.useRef<HTMLInputElement | null>(null);
   const formRef = React.useRef<HTMLFormElement | null>(null);
+  const [files, setFiles] = useState<File[]>([]);
 
   const onClickHandler = () => {
     fileInputRef.current?.click();
   };
 
+  useEffect(() => {
+    console.log(files);
+  }, [files]);
+
   const onChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (!event.target.files?.length) {
       return;
     }
-
-    const formData = new FormData();
-
-    Array.from(event.target.files).forEach((file) => {
-      formData.append(event.target.name, file);
-    });
-
-    props.onChange(formData);
-
-    formRef.current?.reset();
+    setFiles([...Array.from(event.target.files)]);
   };
 
   return (
     <form ref={formRef}>
-      <Button colorScheme="purple" type="button" onClick={onClickHandler}>
-        {props.label}
-      </Button>
-      <Input
-        accept={props.acceptedFileTypes}
-        multiple={props.allowMultipleFiles}
-        name={props.uploadFileName}
-        onChange={onChangeHandler}
-        ref={fileInputRef}
-        style={{ display: "none" }}
-        type="file"
-      />
+      <VStack alignItems="start" spacing={3}>
+        <Stack direction="row">
+          <Button type="button" size="sm" onClick={onClickHandler}>
+            {props.allowMultipleFiles ? "Choose Files" : "Choose File"}
+          </Button>
+          <Input
+            accept={props.acceptedFileTypes}
+            multiple={props.allowMultipleFiles}
+            name={props.uploadFileName}
+            onChange={onChangeHandler}
+            ref={fileInputRef}
+            style={{ display: "none" }}
+            type="file"
+          />
+          {files.length &&
+            (props.allowMultipleFiles ? (
+              <Text>
+                {files[0].name} and {files.length - 1} others
+              </Text>
+            ) : (
+              <Text>{files[0].name}</Text>
+            ))}
+        </Stack>
+        <Button
+          type="button"
+          colorScheme="purple"
+          onClick={() => {
+            const formData = new FormData();
+            files.forEach((file) => {
+              formData.append(props.uploadFileName, file);
+            });
+            props.onChange(formData);
+            formRef.current?.reset();
+            setFiles([]);
+          }}
+        >
+          {props.label}
+        </Button>
+      </VStack>
     </form>
   );
 };
